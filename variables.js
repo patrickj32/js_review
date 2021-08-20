803462a0865e7c42fa60556daa46331e0f853c5f
Patrick Justice <patrickjustice@Patricks-MacBook-Pro.local>
2021-08-20 19:42:42 -0400

    completed module 1

--- variables.js
+++ variables.js
@@ -1,1 +1,34 @@
 console.log("variables.js");
+
+// 1.
+let dog = "Carl";
+const legs = 4;
+var color = "black";
+
+// 2
+console.log(dog);
+console.log(legs);
+console.log(color);
+
+// 3
+
+dog = "Chad";
+color = "brown";
+console.log(dog);
+console.log(color);
+
+// 4
+// What happens if you try to change the value of the variable declared with the
+// const and print its new value to the console?
+
+// Answer: Uncaught TypeError: Assignment to constant variable.
+
+// 5
+// Why might you choose to use the const keyword?
+
+// Answer: if you want something to be the same value without ever changing
+
+// 6
+// What is one reason it not best practice to use the var keyword?
+
+// Answer: You dont get the errors you expect
