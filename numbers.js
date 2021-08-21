0ca2283bc69fff250944c2687cd96f866e4cb7d4
Patrick Justice <patrickjustice@Patricks-MacBook-Pro.local>
2021-08-21 16:12:11 -0400

    completed numbers.js

---
+++ numbers.js
@@ -0,0 +1,28 @@
+console.log("numbers.js");
+
+// 1
+
+let pizza = 6;
+console.log(pizza);
+
+// 2
+
+pizza += 15;
+console.log(pizza);
+
+// 3
+eat = 8;
+
+console.log(pizza - 8);
+
+// 4
+let avg = (26 + 44 + 72 + 86 + 112) / 5;
+console.log(avg);
+
+// 5
+
+let theNumber = 14;
+console.log(++theNumber);
+
+// 6
+typeof number;
